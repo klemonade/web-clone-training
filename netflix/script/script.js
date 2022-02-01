b97489e7ba324176de930ebde88024edfa05ff75
klemonade <upupup2543@gmail.com>
2022-02-01 15:24:32 +0700

    faq text and button with toggle function

--- netflix/script/script.js
+++ netflix/script/script.js
@@ -1,1 +1,15 @@
-fucntion s
+function toggleFAQ(number){
+    faq_id = `faq-${number}`
+    drop_id = `drop-${number}`
+    if (document.getElementById(faq_id).style.display == 'block'){
+    // if (document.getElementById(faq_id).style.height == '0px'){
+        document.getElementById(faq_id).style.display = 'none'
+        // document.getElementById(faq_id).style.height = 'auto'
+        document.getElementById(drop_id).innerHTML = '+'
+    }
+    else {
+        document.getElementById(faq_id).style.display = 'block'
+        // document.getElementById(faq_id).style.height = '0px'
+        document.getElementById(drop_id).innerHTML = 'X'
+    }
+}
