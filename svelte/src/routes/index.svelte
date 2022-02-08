0df068758926b6c08af681aeabe4d16c386f5666
klemonade <upupup2543@gmail.com>
2022-02-08 22:17:44 +0700

    init new svelt

---
+++ svelte/src/routes/index.svelte
@@ -0,0 +1,50 @@
+
+<body class="m-0 p-0 w-full">
+    <header class="header-bg">
+        <nav class="m-4 w-full inline-block">
+            <a href="#">
+                <img class="float-left h-12 w-auto m-2" src="logo.png" alt="img not found"></a>
+            <div class="float-right m-4">
+                <button class="bg-gray-800 text-white p-2 mx-2 rounded">
+                    English</button>
+                <button class="bg-red-600 text-white p-2 mx-2 rounded">
+                    Sign In</button>
+            </div>
+        </nav>
+        <div class="mx-auto relative block top-36 text-center" style="width: 900px;">
+            <h1 class="w-3/4 text-6xl font-bold mx-auto text-white">
+                Unlimited movies, TV shows, and more.</h1>
+            <h3 class="text-2xl font-medium mx-auto my-5 text-white">
+                Watch anywhere. Cancel anytime.</h3>
+            <h4 class="text-lg font-normal mx-auto my-5 text-white">
+                Ready to watch? Enter your email to create or restart your membership.</h4>
+            <div class="h-10">
+                <input type="text" name="email" id="" class="h-16 w-3/5 m-0 indent-3">
+                <button class="text-lg font-medium bg-red-600 text-white w-36 h-16 m-0">
+                    Get Started ></button>
+            </div>
+        </div>
+    </header>
+    <div class="section-line"></div>
+    <div class="h-96 w-screen">
+
+    </div>
+    <div class="section-line"></div>
+</body>
+
+<style>
+    .header-bg{
+        height: 700px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-image:
+            linear-gradient(to bottom, #000000DD, #00000040),
+            url("../assets/background.jpg");
+    }
+    .section-line{
+        width: 100%;
+        background-color: rgb(114, 114, 114);
+        height: 5px;
+    }
+
+</style>
